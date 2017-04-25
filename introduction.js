a11ff81ec2e83447165a422b8fdc7d5ef6bd3529
RDP <rdupaw@gmail.com>
2017-04-24 21:41:56 -0500

    Add javascripting introductions instructions

--- introduction.js
+++ introduction.js
@@ -1,1 +1,43 @@
+/* # JAVASCRIPTING  
+   
+ ## INTRODUCTION (Exercise 1 of 19)  
+   
+  To keep things organized, let's create a folder for this workshop.  
+   
+  Run this command to make a directory called javascripting (or something  
+  else if you like):  
+   
+     mkdir javascripting  
+   
+  Change directory into the javascripting folder:  
+   
+     cd javascripting  
+   
+  Create a file named introduction.js:  
+   
+     touch introduction.js  
+   
+  Or if you're on Windows:  
+   
+     type NUL > introduction.js  
+   
+  (type is part of the command!)  
+   
+  Open the file in your favorite editor, and add this text:  
+   
+     console.log('hello');  
+   
+  Save the file, then check to see if your program is correct by running  
+  this command:  
+   
+     javascripting verify introduction.js  
+   
+  By the way, throughout this tutorial, you can give the file you work  
+  with any name you like, so if you want to use something like  
+  catsAreAwesome.js file for every exercise, you can do that. Just make  
+  sure to run:  
+   
+     javascripting verify catsAreAwesome.js  
+*/
+
 console.log('hello');
