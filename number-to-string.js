114f302fed57649cc9b2b395e491b851ca5307b2
RDP <rdupaw@gmail.com>
2017-04-24 21:52:25 -0500

    Add javascripting rules to each challange for each file

--- number-to-string.js
+++ number-to-string.js
@@ -1,3 +1,31 @@
+/*
+# JAVASCRIPTING  
+   
+ ## NUMBER TO STRING (Exercise 8 of 19)  
+   
+  Sometimes you will need to turn a number into a string.  
+   
+  In those instances you will use the .toString() method. Here's an  
+  example:  
+   
+     var n = 256;  
+     n = n.toString();  
+   
+ ## The challenge:  
+   
+  Create a file named number-to-string.js.  
+   
+  In that file define a variable named n that references the number 128;  
+   
+  Call the .toString() method on the n variable.  
+   
+  Use console.log() to print the results of the .toString() method to the  
+  terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+     javascripting verify number-to-string.js  */
+
 var n = 128;
 
 n = n.toString();
--- strings.js
+++ strings.js
@@ -1,3 +1,39 @@
+/*
+# JAVASCRIPTING  
+   
+ ## STRINGS (Exercise 3 of 19)  
+   
+  A string is a sequence of characters. A character is, roughly speaking,  
+  a written symbol. Examples of characters are letters, numbers,  
+  punctuation marks, and spaces.  
+   
+  String values are surrounded by either single or double quotation  
+  marks.  
+   
+     'this is a string'  
+       
+     "this is also a string"  
+   
+ ## NOTE  
+   
+  Try to stay consistent. In this workshop we'll only use single  
+  quotation marks.  
+   
+ ## The challenge:  
+   
+  For this challenge, create a file named strings.js.  
+   
+  In that file create a variable named someString like this:  
+   
+     var someString = 'this is a string';  
+   
+  Use console.log to print the variable someString to the terminal.  
+   
+  Check to see if your program is correct by running this command:  
+   
+  javascripting verify strings.js  
+*/
+
 var someString = 'this is a string';
 
 console.log(someString);
